f9101990b67432f371b212e5ab507ed042081532
glrmrissi <guilhermerissi04112006@gmail.com>
2026-02-09 13:00:31 -0300

    feat: thottler and cqrs

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,10 +9,11 @@ import { MulterModule } from '@nestjs/platform-express';
 import { AuthGuard } from './guards/auth.guard';
 import { APP_GUARD } from '@nestjs/core/constants';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserEntity } from './shared/UserEntity';
+import { UserEntity } from './shared/entities/UserEntity';
 import { UsersModule } from './modules/users/users.module';
 import { NotifierModule } from './shared/notifier/notifier.module';
 import { BullModule } from '@nestjs/bullmq';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
@@ -38,6 +39,23 @@ import { BullModule } from '@nestjs/bullmq';
         host: process.env.REDIS_HOST,
         port: Number(process.env.REDIS_PORT) || 6379,
       },
+      defaultJobOptions: {
+        removeOnComplete: 100,
+        removeOnFail: 1000,
+        attempts: 5,
+        backoff: {
+          type: 'exponential',
+          delay: 1000,
+        }
+      },
+    }),
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000,
+          limit: 10,
+        },
+      ],
     }),
     AuthModule,
     ApiModule,
@@ -50,6 +68,11 @@ import { BullModule } from '@nestjs/bullmq';
     {
       provide: APP_GUARD,
       useClass: AuthGuard,
-    }],
+    },
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard
+    }
+  ],
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import "reflect-metadata";
+import { LogPerformanceInterceptor } from './interceptors/performance.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  
   const logger = new Logger('Bootstrap');
   app.enableCors(
     {
@@ -14,13 +15,17 @@ async function bootstrap() {
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     }
   );
-
+  
   const config = new DocumentBuilder()
     .setTitle('Whisker Anime API')
     .setDescription('The Whisker Anime API description')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
+  
+
+  app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalInterceptors(new LogPerformanceInterceptor)
   SwaggerModule.setup('api-docs', app, document);
 
   await app.listen(process.env.PORT ?? 3001);
