5e51651461cb0cef200b3787f436eff279481938
glrmrissi <guilhermerissi04112006@gmail.com>
2026-03-04 05:04:35 -0300

    chore: remove console.log

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -26,7 +26,6 @@ export class AuthGuard implements CanActivate {
     }
 
     const request = context.switchToHttp().getRequest();
-    console.log(request);
     const token = this.extractTokenFromHeader(request);
     if (!token) {
       throw new UnauthorizedException();
